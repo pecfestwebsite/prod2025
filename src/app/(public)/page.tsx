1bb653441d2bffa82ba3980d30266695e6290dd5
Knight-Fenris <ribhavaggarwal29@gmail.com>
2025-10-25 23:33:36 +0530

    finaljsr

--- src/app/(public)/page.tsx
+++
@@ -1,7 +0,0 @@
-import LandingPage from "../../components/pages/landingpage";
-
-export default function Home() {
-  return (
-    <LandingPage />
-  );
-}
--- src/app/api/auth/callback/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-export async function GET(request: NextRequest) {
-  // Let the Auth0 React SDK handle the callback
-  // This route should just redirect to the main app
-  // The Auth0 React SDK will handle the token exchange automatically
-  return NextResponse.redirect(new URL('/', request.url));
-}
